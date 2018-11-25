afe2345b79b19f5c30f5e57f6073391903c15529
Esteban D. Barboza Primera <dario0117@gmail.com>
2018-11-24 23:07:15 -0500

    styled modals

--- frontend/src/components/Conversation.jsx
+++ frontend/src/components/Conversation.jsx
@@ -1,29 +1,90 @@
 import React, { Component } from 'react';
-import { Menu, Dropdown, Icon, Modal } from 'antd';
+import {
+    Menu, Dropdown, Icon,
+    Modal, Select, Input, List
+} from 'antd';
 import './Conversation.css';
 
+const Option = Select.Option;
+const Search = Input.Search;
 
 class Conversation extends Component {
-    state = {
-        NewCmodalVisible: false,
-        SearchCmodalVisible: false,
-        conversations: [
-            'Name 1',
-            'Name 2',
-            'Name 3',
-            'Name 4',
-            'Name 5',
-            'Name 6',
-            'Name 7',
-            'Name 8',
-            'Name 9',
-            'Name 10',
-        ]
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            NewCmodalVisible: false,
+            SearchCmodalVisible: false,
+            results:  ['asd', 'asd2'],
+            conversations: [
+                'Name 1',
+                'Name 2',
+                'Name 3',
+                'Name 4',
+                'Name 5',
+                'Name 6',
+                'Name 7',
+                'Name 8',
+                'Name 9',
+                'Name 10',
+            ],
+            users: [
+                {
+                    id: 5,
+                    name: 'Name 1'
+                },
+                {
+                    id: 312,
+                    name: 'Name 2'
+                },
+                {
+                    id: 3534,
+                    name: 'Name 3'
+                },
+                {
+                    id: 646,
+                    name: 'Name 4'
+                },
+                {
+                    id: 2342,
+                    name: 'Name 5'
+                },
+                {
+                    id: 75657,
+                    name: 'Name 6'
+                },
+                {
+                    id: 7878,
+                    name: 'Name 7'
+                },
+            ],
+        }
+        this.children = [];
+        for (let user of this.state.users) {
+            this.children.push(<Option key={user.name}>{user.name}</Option>);
+        }
+
+        // Create new conversation fields
+        this.selectedUsers = [];
+        this.convName = "";
+
     }
 
-    setNewCVisible = (NewCmodalVisible) => {
-        this.setState({ NewCmodalVisible });
+    NewCOk = () => {
+        console.log("Creating conv...");
+        this.selectedUsers = [];
+        this.convName = "";
+        this.setState({ NewCmodalVisible: false });
+        window.location.reload(true);
     }
+
+    NewCCancel = () => {
+        this.selectedUsers = [];
+        this.convName = "";
+        this.setState({ NewCmodalVisible: false });
+        window.location.reload(true);
+    }
+
     setSearchCVisible = (SearchCmodalVisible) => {
         this.setState({ SearchCmodalVisible });
     }
@@ -34,6 +95,22 @@ class Conversation extends Component {
         } else if (e.key === '1') {
             this.setState({ SearchCmodalVisible: true });
         }
+    }
+
+    handleChange = (value) => {
+        this.selectedUsers = value;
+        // console.log(this.selectedUsers);
+    }
+
+    ConvNameChange = (e) => {
+        this.convName = e.target.value;
+    }
+
+    searchConv = (value) => {
+        console.log(value)
+        this.setState({ 
+            results: this.state.results.concat(value) 
+        });
     }
 
     render() {
@@ -60,12 +137,24 @@ class Conversation extends Component {
                     title="Create a new conversation"
                     centered
                     visible={this.state.NewCmodalVisible}
-                    onOk={() => this.setNewCVisible(false)}
-                    onCancel={() => this.setNewCVisible(false)}
+                    onOk={() => this.NewCOk()}
+                    onCancel={() => this.NewCCancel()}
                 >
-                    <p>some contents...</p>
-                    <p>some contents...</p>
-                    <p>some contents...</p>
+                    <span>Select all the users you want in this conversation:</span> <br />
+                    <span>* NOTE: If you select only one, it means that you are creating a private message.</span>
+                    <Select
+                        mode="multiple"
+                        style={{ width: '100%' }}
+                        placeholder="Please select users"
+                        allowClear={true}
+                        defaultValue={[]}
+                        onChange={this.handleChange}
+                    >
+                        {this.children}
+                    </Select>
+                    <p></p>
+                    <span>Insert the name of the conversation:</span>
+                    <Input onChange={this.ConvNameChange} placeholder="Conversation name" />
                 </Modal>
 
                 <Modal
@@ -75,9 +164,17 @@ class Conversation extends Component {
                     onOk={() => this.setSearchCVisible(false)}
                     onCancel={() => this.setSearchCVisible(false)}
                 >
-                    <p>some contents...</p>
-                    <p>some contents...</p>
-                    <p>some contents...</p>
+                    <Search
+                        placeholder=""
+                        onSearch={this.searchConv}
+                        style={{ width: '100%' }}
+                    />
+                    <p></p>
+                    <List
+                        bordered
+                        dataSource={this.state.results}
+                        renderItem={item => (<List.Item onClick={() => console.log("les ge")}>{item}</List.Item>)}
+                    />
                 </Modal>
             </div>
         )
